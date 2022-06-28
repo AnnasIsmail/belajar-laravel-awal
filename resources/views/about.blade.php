803336e02a583e31860ba496bf6e0bac2e49adbe
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-06-29 02:21:55 +0700

    Update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Coba Blade</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    <h1>{{ $name }}</h1>
+    <h1 class="coba" >{{ $name }}</h1>
     <h2>{{ $email }}</h2>
 </body>
 </html>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Account Community</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  </head>
+    <link rel="stylesheet" href="css/main.css">
+</head>
   <body>
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -29,6 +30,16 @@
           </div>
         </div>
       </nav>
+
+      <div class="input-group input-group-sm mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Email</span>
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+      </div>
+
+      <div class="input-group input-group-sm mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Password</span>
+        <input type="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+      </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   </body>
